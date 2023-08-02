58f796d83b1622fd6f356db3e5ed38aa9c0fe789
Divya-nyati1206 <111676966+Divya-nyati1206@users.noreply.github.com>
2023-08-02 12:49:16 +0530

    Add files via upload

---
+++ alien_invasion/settings.py
@@ -0,0 +1,24 @@
+class Settings():
+
+    def __init__(self):
+
+        self.screen_width=1500
+        self.screen_height=770
+        self.bg_color=(0,0,0)
+
+        self.ship_speed_factor=1.5
+        self.ship_limit=3
+
+        self.bullet_speed_factor=3
+        self.bullet_width=5
+        self.bullet_height=20
+        self.bullet_color=(255,255,255)
+        self.bullets_allowed=5
+
+        self.alien_speed_factor=1
+        self.fleet_drop_speed=10
+        self.fleet_direction=1           # 1 for right and -1 for left
+
+        self.alien_points=10
+
+        
